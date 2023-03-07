3d8e4e364f7cf969be1894e6189b0f5fc59cc7c6
Oluwanifise Oguntoye <117686447+oluwanifiseoguntoye@users.noreply.github.com>
2023-03-07 15:08:00 -0500

    Basic UI setup. now Dialogflow & Chatbot

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [messages, setMessages] = useState([]);
+  const messagesContainerRef = useRef(null);
 
   const handleMessageSubmit = (event) => {
     event.preventDefault();
@@ -11,18 +12,24 @@ function App() {
     if (message !== '') {
       setMessages((prevMessages) => [...prevMessages, { text: message, sender: 'user' }]);
       messageInput.value = '';
-      const messagesContainer = document.querySelector('.messages');
-      messagesContainer.scrollTop = messagesContainer.scrollHeight;
     }
   };
+
+  useEffect(() => {
+    const messagesContainer = messagesContainerRef.current;
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  }, [messages]);
 
   return (
     <div className="App">
       <div className="chat-wrapper">
         <div className="chat-container">
-          <div className="messages">
+          <div className="messages" ref={messagesContainerRef}>
             {messages.map((message, index) => (
               <div key={index} className={`message ${message.sender}`}>
+                <span className={`icon ${message.sender === 'user' ? 'user-icon' : 'bot-icon'}`}>
+                  <i className={`fas ${message.sender === 'user' ? 'fa-user' : 'fa-robot'}`}></i>
+                </span>
                 <div className="message-text">{message.text}</div>
               </div>
             ))}
